3e8f5b768e5abae09daafe3faca8cb1174eedb32
viniciosComerciante <vinicompif@gmail.com>
2022-03-10 00:30:42 -0300

    Header component

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo ignews" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
