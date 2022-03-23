eb69619c5ec18fb1f361df6d5eb834edb0808b31
viniciosComerciante <vinicompif@gmail.com>
2022-03-22 23:54:21 -0300

    listados posts por meio da api do prismic

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,10 +1,22 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import * as prismicio from "@prismicio/client";
+import { RichText } from "prismic-dom";
 import prismic from "../../services/prismic";
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,54 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn workspace</strong>
-            <p>
-              In this guide, you will lean hot to create a Monorepo to manage
-              Multiple packages with a share button without a memory man stage
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn workspace</strong>
-            <p>
-              In this guide, you will lean hot to create a Monorepo to manage
-              Multiple packages with a share button without a memory man stage
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn workspace</strong>
-            <p>
-              In this guide, you will lean hot to create a Monorepo to manage
-              Multiple packages with a share button without a memory man stage
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn workspace</strong>
-            <p>
-              In this guide, you will lean hot to create a Monorepo to manage
-              Multiple packages with a share button without a memory man stage
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn workspace</strong>
-            <p>
-              In this guide, you will lean hot to create a Monorepo to manage
-              Multiple packages with a share button without a memory man stage
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn workspace</strong>
-            <p>
-              In this guide, you will lean hot to create a Monorepo to manage
-              Multiple packages with a share button without a memory man stage
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -73,13 +44,34 @@ export const getStaticProps: GetStaticProps = async () => {
   // });
 
   const init = async () => {
-    const prismicDoc = await prismic.getAllByType("publication");
-    console.log(prismicDoc);
+    const prismicDoc = await prismic.getAllByType("publication", {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    });
+    return prismicDoc;
   };
 
-  init();
+  const response = await init();
+
+  const posts = response.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
