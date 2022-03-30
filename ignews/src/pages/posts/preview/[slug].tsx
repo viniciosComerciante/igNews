ec5a6c45f6292680407d3288051a2216ec9edb7c
viniciosComerciante <vinicompif@gmail.com>
2022-03-29 23:13:21 -0300

    pagina de preview de post e verificaçao se o usuario nao e assinante para que um assinante nunca acesse a pagina de preview e sim a pagina inteira

---
+++ ignews/src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,91 @@
+import { GetStaticProps } from "next";
+import { getSession, useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { RichText } from "prismic-dom";
+import { useEffect } from "react";
+import prismic from "../../../services/prismic";
+import styles from "../post.module.scss";
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  console.log(session);
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session, post.slug, router]);
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          ></div>
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">Subscribe now 😍</Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const init = async () => {
+    const prismicDoc = await prismic.getByUID("publication", String(slug), {});
+    return prismicDoc;
+  };
+
+  const response = await init();
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updatedAt: new Date(response.last_publication_date).toLocaleString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
