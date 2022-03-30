20bdf8bd3ab4bb01c8d7416fadbebe0da43ff379
viniciosComerciante <vinicompif@gmail.com>
2022-03-29 23:26:47 -0300

    gerando pagina estatica em tempo de build

--- ignews/src/pages/posts/preview/[slug].tsx
+++ ignews/src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { getSession, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -52,9 +52,15 @@ export default function PostPreview({ post }: PostPreviewProps) {
   );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [],
+    paths: [
+      {
+        params: {
+          slug: "python-linguagem-popular-com-berco-na-comedia-inglesa",
+        },
+      },
+    ],
     fallback: "blocking",
   };
 };
@@ -87,5 +93,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
+    revalidate: 60 * 30, //30 minutos
   };
 };
